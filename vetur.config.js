ef971caf7c95e216898bd642505ba76f7f491ca6
piteroni <dev.piteroni@gmail.com>
2021-11-04 02:00:43 +0900

    脱vetur

--- vetur.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  settings: {
-    "vetur.useWorkspaceDependencies": true,
-    "vetur.experimental.templateInterpolationService": true
-  },
-  projects: [
-    { root: "./dictionary-application-ui" }
-  ]
-}
