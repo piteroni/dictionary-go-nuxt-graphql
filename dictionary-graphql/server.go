0ca82c04c01f59b8fba5840db702b280531e193d
piteroni <dev.piteroni@gmail.com>
2021-11-05 03:33:57 +0900

    v5を作成

--- dictionary-graphql/server.go
+++ dictionary-graphql/server.go
@@ -34,8 +34,8 @@ func main() {
 	router := mux.NewRouter()
 
 	// FOR ECS DEBUG.
-	router.HandleFunc("/ping-v4", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong-v4"))
+	router.HandleFunc("/ping-v5", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong-v5"))
 	})
 	router.Handle("/api/i/query", srv)
 	router.Handle("/", playground.Handler("GraphQL playground", "/api/i/query"))
